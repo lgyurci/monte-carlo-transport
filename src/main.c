4033446afd8292234b2a2c33de4c1162ce62a457
Lázár György <lgyurci@gmail.com>
2022-06-06 19:51:12 +0200

    added multi-threading and real time graphical display with gnuplot

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <sys/time.h>
 #include "reactions.h"
 #include "mtwister.h"
+#include <pthread.h>
 
 #define inf 1e9
 #define pi 3.14159
@@ -14,9 +15,18 @@ double pztop = 2;
 double pzbottom = -2;
 double radius = 2;
 
-int ch[channels] = { 0 };
-
 double maxenergy = 4100;
+
+struct tracingThreadArgs{
+    MTRand *random;
+    int *chan;
+    int stop_at_colls;
+    double sourceEnergy;
+    double cosalpha;
+    double sigma;
+    struct vector sourcePos;
+    int particlesTraced;
+};
 
 //double ro = 3.67;
 
@@ -59,17 +69,54 @@ double traceParticle(struct vector pos, struct vector direction, double particle
     return sumen;
 }
 
+void plot(FILE *gp_pipe,int *chan, int chansize){
+    fprintf(gp_pipe,"plot '-'\n");
+    for (int i = 0; i < chansize; i++){
+        fprintf(gp_pipe,"%d %d\n",i,chan[i]);
+        //printf("%d %f\n",i,ch[i]);
+    }
+    fprintf(gp_pipe,"e\n");
+}
+
+void *tracingThread(void* arg){
+    struct tracingThreadArgs *targs = (struct tracingThreadArgs*) arg;
+
+    int colledParticles = 0;
+    int i = 0;
+
+    for (; colledParticles < targs->stop_at_colls; i++){
+        //struct vector direction = isotropicDirection();
+        struct vector direction = coneDirection(targs->cosalpha,vMult(targs->sourcePos,-1),targs->random);
+        double firstPoint = intersect_cylinder_out(targs->sourcePos,direction,pztop,pzbottom,radius);
+        if (firstPoint < inf){
+            double energyAbsorbed = traceParticle(transloc(targs->sourcePos,vMult(direction,firstPoint)),direction,targs->sourceEnergy,targs->random);
+            if (energyAbsorbed > 0) {
+                colledParticles++;
+                energyAbsorbed = energyAbsorbed + targs->sigma*drandnt(targs->random);
+                if (maxenergy < energyAbsorbed) energyAbsorbed = maxenergy;
+                (targs->chan)[((int)(energyAbsorbed*(channels-1)/maxenergy))]++;
+            }
+        }
+    }
+    targs->particlesTraced += i;
+}
+
 int main(){
   //  initRandrand();
     double roh = 3.67;
     initReactions(roh);
 
-    int particleNum = 5e5;
+    int particleNum = 5e8;
+    int update = 1e4;
     double sourceEnergy = 4000;
     double fwhm = 30;
     double sigma = fwhm/2.355;
 
     MTRand mainrand = initRandrandt();
+
+    FILE* gp_pipe = popen ("gnuplot -persistent", "w");
+    fprintf(gp_pipe,"set title 'Spectrum'\n");
+    fprintf(gp_pipe,"set logscale y\n");
 
     struct vector sourcePos;
     sourcePos.x = 3;
@@ -79,32 +126,90 @@ int main(){
     double sinalpha = sqrt(radius*radius+(pztop-pzbottom)*(pztop-pzbottom))/vAbs(sourcePos);
     double cosalpha = sqrt(1-sinalpha*sinalpha);
 
-    //struct timeval ct; //idő lekérésere szolgáló struct
-    //gettimeofday(&ct,NULL);
-    //long peTi = ct.tv_sec * (int)1e6 + ct.tv_usec;
-    int colledParticles = 0;
+    int *sumTChannels = malloc(sizeof(int)*channels);
+  /*  for (int i = 0; i < channels; i++){
+        sumTChannels[i] = 0;
+    }*/
 
-    for (int i = 0; colledParticles < particleNum; i++){
-        //struct vector direction = isotropicDirection();
-        struct vector direction = coneDirection(cosalpha,vMult(sourcePos,-1),&mainrand);
-        double firstPoint = intersect_cylinder_out(sourcePos,direction,pztop,pzbottom,radius);
-        if (firstPoint < inf){
-            double energyAbsorbed = traceParticle(transloc(sourcePos,vMult(direction,firstPoint)),direction,sourceEnergy,&mainrand);
-            if (energyAbsorbed > 0) {
-                colledParticles++;
-                energyAbsorbed = energyAbsorbed + sigma*drandnt(&mainrand);
-                if (maxenergy < energyAbsorbed) energyAbsorbed = maxenergy;
-                ch[((int)(energyAbsorbed*(channels-1)/maxenergy))]++;
+    int threadCount = 4;
+
+    int **threadChannels = malloc(sizeof(int*)*threadCount);
+    struct tracingThreadArgs *targs = malloc(sizeof(struct tracingThreadArgs)*threadCount);
+    MTRand *threadRands = malloc(sizeof(MTRand)*threadCount);
+    pthread_t *threads = malloc(sizeof(pthread_t)*threadCount);
+
+    struct timeval ct;
+    gettimeofday(&ct,NULL);
+    long peTi = ct.tv_sec * (int)1e6 + ct.tv_usec;
+
+    for (int i = 0; i < threadCount; i++){
+        threadChannels[i] = malloc(sizeof(int)*channels);
+        for (int j = 0; j < channels; j++){
+            threadChannels[i][j] = 0;
+        }
+        threadRands[i] = initRandt((long)(drandt(&mainrand)*1e6));
+        targs[i].chan = threadChannels[i];
+        targs[i].random =  &(threadRands[i]);
+        targs[i].stop_at_colls = update;
+        targs[i].sourceEnergy = sourceEnergy;
+        targs[i].cosalpha = cosalpha;
+        targs[i].sigma = sigma;
+        targs[i].sourcePos = sourcePos;
+        targs[i].particlesTraced = 0;
+        pthread_create(&(threads[i]),NULL,tracingThread,&(targs[i]));
+    }
+
+    int sumcoll = 0;
+    int sumpart = 0;
+
+    for (int i = 0; i < particleNum/update; i++){
+
+        for (int j = 0; j < threadCount; j++){
+            pthread_join(threads[j],NULL);
+        }
+
+        for (int j = 0; j < threadCount; j++){
+            sumpart += targs[j].particlesTraced;
+        }
+
+        for (int j = 0; j < channels; j++){
+            sumTChannels[j] = 0;
+            for (int k = 0; k < threadCount; k++){
+                sumTChannels[j] += threadChannels[k][j];
+                sumcoll += threadChannels[k][j];
             }
         }
+        for (int j = 0; j < threadCount; j++){
+            pthread_create(&(threads[j]),NULL,tracingThread,&(targs[j]));
+        }
+
+        //Minden más kód ide jön!!!!
+
+        gettimeofday(&ct,NULL);
+
+  //      printf ("Average speed: %f kp/s\n",sumcoll/1e3/((double)(ct.tv_sec * (int)1e6 + ct.tv_usec - peTi)/1e6));
+  //      printf ("Total collisions: %d\n",sumcoll);
+        fprintf(gp_pipe,"set label 1 'Average speed: %.1f kp/s' at screen 0.05,screen 0.98 \n",sumcoll/1e3/((double)(ct.tv_sec * (int)1e6 + ct.tv_usec - peTi)/1e6));
+        fprintf(gp_pipe,"set label 2 'Total number of hits: %.3f M' at screen 0.95,screen 0.98 right\n",sumcoll/1e6);
+        fprintf(gp_pipe,"set label 3 'Total particles traced: %.3f M' at screen 0.05,screen 0.95 \n",(double) sumpart/1e6);
+       // fprintf(gp_pipe,"set label 4 'Total number of hits: %.3f M' at screen 0.95,screen 0.98 right\n",sumcoll/1e6);
+//        fprintf(gp_pipe,"set logscale y\n");
+
+        plot(gp_pipe,sumTChannels,channels);
+
+        sumcoll = 0;
+        sumpart = 0;
+
     }
+
 
     //gettimeofday(&ct,NULL);
     //printf ("Average speed: %f Mp/s\n",colledParticles/1e6/((double)(ct.tv_sec * (int)1e6 + ct.tv_usec - peTi)/1e6));
 
-    for (int i = 0; i < channels; i++){
+ /*   for (int i = 0; i < channels; i++){
         printf("%d\n",ch[i]);
-    }
+    }*/
+   // plot(gp_pipe);
     freeReactions();
     return 0;
 }
