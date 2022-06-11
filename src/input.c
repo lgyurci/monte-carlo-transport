c4bf915a11418c4ed05d2b6411bb6474ea9d74c5
Lázár György <lgyurci@gmail.com>
2022-06-11 09:12:25 +0200

    Added a lot more input parsing

--- src/input.c
+++ src/input.c
@@ -32,6 +32,53 @@ void setDefaultInputs(struct input_data * id){
     copySFromInto("crs_NaI",id->xcomLocation);
 }
 
+double parseDouble(char *line){
+    int ol = 0;
+    char tc[20] = {0};
+    int i;
+    for(i = 0; line[ol] != ';' && line[ol] != '\0'; ol++){
+        tc[i++] = line[ol];
+    }
+    tc[i] = '\0';
+    return atof(tc);
+}
+
+int parseInt(char *line){
+    int ol = 0;
+    char tc[20] = {0};
+    int i;
+    for(i = 0; line[ol] != ';' && line[ol] != '\0'; ol++){
+        tc[i++] = line[ol];
+    }
+    tc[i] = '\0';
+    return atoi(tc);
+}
+
+long parseLong(char *line){
+    int ol = 0;
+    char tc[20] = {0};
+    int i;
+    for(i = 0; line[ol] != ';' && line[ol] != '\0'; ol++){
+        tc[i++] = line[ol];
+    }
+    tc[i] = '\0';
+    return strtol(tc);
+}
+
+
+
+void parseString(char *line,char *tc){
+    int ol = 0;
+    int i;
+    for(i = 0; line[ol] != ';' && line[ol] != '\0'; ol++){
+        tc[i++] = line[ol];
+    }
+    tc[i] = '\0';
+}
+
+char *parseVector(char *tc, int ol){
+}
+
 
 struct input_data getInput(int argc,char** argv){
     struct input_data id;
@@ -60,22 +107,58 @@ struct input_data getInput(int argc,char** argv){
                 option[ol] = line[ol];
             }
             option[ol] = '\0';
+            char tc[20] = {0};
 
-            if (strcmp(option,"RTThreads") == 0) {
-                char tc [6];
-                for (int i = 0; i < 6; i++){
-                    tc[i] = 0;
-                }
-                ol++;
-                int i;
-                for(i = 0; line[ol] != ';' && line[ol] != '\0'; ol++){
-                    tc[i++] = line[ol];
-                }
-                tc[i] = '\0';
-                tcount = atoi(tc);
-                if (tcount == 0) {
+            if (strcmp(option,"Thread count") == 0) {
+                id->threadCount = parseInt(line[ol++]);
+                if (id->threadCount <= 0) {
                     exit(0);
                 }
+            }
+
+            if (strcmp(option,"Channels") == 0) {
+                id->channels = parseInt(line[ol++]);
+                if (id->channels <= 0) {
+                    exit(0);
+                }
+            }
+
+            if (strcmp(option,"Stop when collisions reach") == 0) {
+                id->particleNum = parseLong(line[ol++]);
+                if (id->particleNum < 0) {
+                    exit(0);
+                }
+            }
+
+            if (strcmp(option,"Real time plotting with gnuplot") == 0) {
+                int b = parseInt(line[ol++]);
+                if (b == 0 || b == 1) id->realtime = b;
+            }
+
+            if (strcmp(option,"Density (g/cm3)") == 0) {
+                id->rho = parseDouble(line[ol++]);
+                if (id->rho <= 0) exit(0);
+            }
+
+            if (strcmp(option,"FWHM") == 0) {
+                id->sigma = parseDouble(line[ol++]);
+                if (id->sigma < 0) exit(0);
+            }
+
+            if (strcmp(option,"Energy of the source") == 0) {
+                id->sourceEnergy = parseDouble(line[ol++]);
+                if (id->sourceEnergy <= 0) exit(0);
+            }
+
+            if (strcmp(option,"Update real time every N collisions") == 0) {
+                id->updateFreq = parseLong(line[ol++]);
+                if (id->updateFreq < 0) exit(0);
+            }
+
+            if (strcmp(option,"gnuplot executable") == 0) {
+                char tc [255] = {0};
+                parseString(line,tc);
+                copySFromInto(tc,id->gnuplotExecutable);
             }
         }
     }
